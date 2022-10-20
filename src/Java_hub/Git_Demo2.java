d38a3e27869081d0ab184d29b5a4effb4e9a196a
sspat <sspat@sona>
2022-10-20 22:50:10 +0530

    Second commit

---
+++ src/Java_hub/Git_Demo2.java
@@ -0,0 +1,14 @@
+package Java_hub;
+
+public class Git_Demo2 {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);
+        int d = a - b;
+        System.out.println(d);
+	}
+
+}
